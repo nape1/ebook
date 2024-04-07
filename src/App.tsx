3c1e107b4e8269a0cd89d8f26a934d1ec5174579
nape1 <najinshah.pearson@gmail.com>
2024-04-07 14:15:34 +0530

    initial app added import aliasing added with changes in viteconfig and tsconfig.json for correct types in path module install @types/node otherwise vite config will show ts errors resolve object in vite config is responsible for path resolving, the same path alisaing is to be added in tsconfig to avoid ts error.if not alisaing works ith ts erros

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import HelloWorld from "@components/HelloWorld"
+
+function App() {
+
+  return (
+    <>
+      <h1>
+         📚 Ebook Readers ...
+      </h1>
+      <HelloWorld />
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+//For path aliasing
+import path from "path"
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve:{
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+})
