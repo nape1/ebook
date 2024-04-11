c79cdb4ec2e782c4ed35e9242359c0eeb78b1530
nape1 <najinshah.pearson@gmail.com>
2024-04-11 22:41:39 +0530

    file loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import FileSelector from "@components/FileSelector/FileSelector"
 import PDF from "@components/PdfViewer/PDF"
+import { useState } from "react"
 
 function App() {
+    const [setselectedFile, setSetselectedFile] = useState<File | null>(null)
   return (
     <>
       <h1>📚 Ebook Readers</h1>
       <div>
-        <PDF />
+        <FileSelector onFileSelect={setSetselectedFile}/>
+        <PDF file={setselectedFile}/>
       </div>
     </>
   )
--- src/components/PdfViewer/PDF.tsx
+++ src/components/PdfViewer/PDF.tsx
@@ -7,7 +7,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 import "react-pdf/dist/esm/Page/TextLayer.css"
 import React from "react"
 
-import './pdf.css';
+import "./pdf.css"
 
 pdfjs.GlobalWorkerOptions.workerSrc = "/js/pdf.worker.min.js"
 
@@ -17,10 +17,7 @@ const maxWidth = 800
 
 type PDFFile = string | File | null
 
-const PDF = () => {
-  const [file, setFile] = useState<PDFFile>(
-    "https://pdftron.s3.amazonaws.com/downloads/pl/Chart_.Vector.pdf"
-  )
+const PDF = ({file}:{file:PDFFile}) => {
   const [numPages, setNumPages] = useState<number>()
   const [containerRef, setContainerRef] = useState<HTMLElement | null>(null)
   const [containerWidth, setContainerWidth] = useState<number>()
@@ -43,42 +40,29 @@ const PDF = () => {
 
   useResizeObserver(containerRef, resizeObserverOptions, onResize)
 
-  function onFileChange(event: React.ChangeEvent<HTMLInputElement>): void {
-    const { files } = event.target
-
-    if (files && files[0]) {
-      setFile(files[0] || null)
-    }
-  }
   function onDocumentLoadSuccess({ numPages: nextNumPages }: unknown): void {
     setNumPages(nextNumPages)
   }
 
   return (
     <div id="Pdf_viewer">
-      <div className="Example__container">
-        <div className="Example__container__load">
-          <label htmlFor="file">Load from file:</label>{" "}
-          <input onChange={onFileChange} type="file" />
-        </div>
-        <div className="Example__container__document" ref={setContainerRef}>
-          <Document
-            file={file}
-            onLoadSuccess={onDocumentLoadSuccess}
-            options={options}
-          >
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-                height={10}
-                width={
-                  containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
-                }
-              />
-            ))}
-          </Document>
-        </div>
+      <div className="Example__container__document" ref={setContainerRef}>
+        <Document
+          file={file}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+        >
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page
+              key={`page_${index + 1}`}
+              pageNumber={index + 1}
+              height={10}
+              width={
+                containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth
+              }
+            />
+          ))}
+        </Document>
       </div>
     </div>
   )
